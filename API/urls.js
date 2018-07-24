c17aa4c66c50238df5e01cf33d8ae81b13d837ba
Jaime Moreno Cantó <jmc103@alu.ua.es>
2018-07-24 20:40:28 +0200

    Referencias de API a producción

--- API/urls.js
+++ API/urls.js
@@ -1,3 +1,2 @@
-// TODO: Change URL for static ip
-export const BASE_URL = 'http://192.168.1.23:3000'
-export const API_URL = 'http://192.168.1.23:3000/api'
+export const BASE_URL = 'https://mighty-reef-55430.herokuapp.com'
+export const API_URL = 'https://mighty-reef-55430.herokuapp.com/api'
